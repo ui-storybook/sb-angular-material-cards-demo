713fef3c3d4d952092bcfdc744f1b16f180c0c57
freeman14 <dubovetzky@me.com>
2016-10-17 01:26:46 +0300

    Update helper loader

--- sb/index.js
+++ sb/index.js
@@ -6,16 +6,22 @@ import app from '../src/index';
 // Remove this to turn off auto reload
 if (module.hot) {
   module.hot.accept();
-} 
- 
+}
+
 const mainModule = angular.module('sb', [
   'helper',
   app
 ]);
 
+// If we have old app remove it
+let oldApp = document.getElementsByTagName('preview-helper')[0];
+
+if (oldApp) {
+  oldApp.remove();
+}
+
 let preview = document.createElement('preview-helper');
 document.body.appendChild(preview);
-
 angular.element(preview)
   .ready(() => {
     angular.bootstrap(preview, [mainModule.name], { strictDi: false });
