9edb2d458b40cccb6954ddd64e8df85142e400c4
freeman14 <dubovetzky@me.com>
2016-10-18 22:19:17 +0300

    Added demo grid component

--- sb/stories/index.js
+++ sb/stories/index.js
@@ -12,4 +12,37 @@ overview.story('Basic card')
     title: 'City of Dreams',
     image: 'https://unsplash.it/400/215/?random',
     withActions: true
-  })
+  });
+
+let cardGrid = `
+  <md-content class="md-padding" layout-xs="column" layout="row">
+      <card ng-repeat="card in vm.cards" title="card.title" image="card.image" with-actions="card.withActions"></card>
+  </md-content>
+`;
+
+let overviewGrid = sb.section('Material cards grid');
+overviewGrid.story('Basic Grid')
+  .add('Grid', cardGrid, {
+    cards: [
+      {
+        title: 'City of Dreams',
+        image: 'https://unsplash.it/400/215/?random',
+        withActions: true
+      },
+      {
+        title: 'City of Dreams',
+        image: 'https://unsplash.it/400/215/?random',
+        withActions: false
+      },
+      {
+        title: 'City of Dreams',
+        image: 'https://unsplash.it/400/215/?random',
+        withActions: true
+      },
+      {
+        title: 'City of Dreams',
+        image: 'https://unsplash.it/400/215/?random',
+        withActions: false
+      }
+    ]
+  });
